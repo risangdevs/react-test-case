f7ce84c6dd74b662d3d16bedf06c4d1bd74b3644
risangdevs <risangdevs@gmail.com>
2022-01-28 11:15:13 +0700

    added fetch news by country feature

--- fe-challenge-eigen/src/components/Navbar/Navbar.jsx
+++ fe-challenge-eigen/src/components/Navbar/Navbar.jsx
@@ -1,18 +1,50 @@
 import React, { useEffect } from "react";
-import { useDispatch } from 'react-redux'
-import { Link, useNavigate } from 'react-router-dom'
-import './Navbar.css'
+import { useDispatch } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import "./Navbar.css";
+import { NavDropdown } from "react-bootstrap";
+import { fetchNewsByCountry } from "../../store/action";
 
 const Navbar = () => {
-
+  const dispatch =useDispatch()
+  const country =(country)=>{
+    dispatch(fetchNewsByCountry(country))
+  }
   return (
-      <ul className="menu-bars">
-          <li className="navbar a"><Link to="/">Home</Link></li>
-          <li className="navbar a"><a href="#">About</a></li>
-          <li className="navbar a"><a href="#">Services</a></li>
-          <li className="navbar a"><a href="#">Team</a></li>
-          <li className="navbar a"><a href="https://www.linkedin.com/in/risanggani/">Contact</a></li>         
-      </ul>
+    <ul className="menu-bars">
+      <li className="navbar a">
+        <Link to="/">Home</Link>
+      </li>
+      <li className="navbar a">
+        <a href="#">About</a>
+      </li>
+      <li className="navbar a">
+        <a href="#">Services</a>
+      </li>
+      <li className="navbar a">
+        <a href="#">Team</a>
+      </li>
+      <li className="navbar a">
+        <a href="https://www.linkedin.com/in/risanggani/">Contact</a>
+      </li>
+      <NavDropdown
+        title="Country"
+        id="collasible-nav-dropdown"
+        className="navbar a"
+      >
+        <NavDropdown.Item href="#action/3.1" onClick={() => country("id")}>
+          Indonesia
+        </NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.2" onClick={() => country("us")}>
+          USA
+        </NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.3" onClick={() => country("ru")}>
+          Russia
+        </NavDropdown.Item>
+        {/* <NavDropdown.Divider />
+        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item> */}
+      </NavDropdown>
+    </ul>
   );
 };
 
--- fe-challenge-eigen/src/store/action.js
+++ fe-challenge-eigen/src/store/action.js
@@ -20,3 +20,21 @@ export const fetchNews = () => {
     }
   };
 };
+export const fetchNewsByCountry = (country) => {
+  return async (dispatch, getState) => {
+    try {
+      dispatch({ type: IS_LOADING, payload: true });
+      const response = await fetch( `https://newsapi.org/v2/top-headlines?country=${country}&apiKey=8e6d0e245c544ae2b66b9e7503de4115`);
+      if (response.ok) {
+        const result = await response.json();
+        dispatch({ type: FETCH_NEWS, payload: result.articles });
+      } else {
+        throw new Error("Fetch News Failed");
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      dispatch({ type: IS_LOADING, payload: false });
+    }
+  };
+};
